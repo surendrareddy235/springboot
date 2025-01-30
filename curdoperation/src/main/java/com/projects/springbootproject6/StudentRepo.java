cdfa2a5919c031f3bdad1d70c16b4b5bfcfa9db1
surendrareddy235 <157972541+surendrareddy235@users.noreply.github.com>
2025-01-30 22:51:47 +0530

    Delete curdoperation directory

--- curdoperation/src/main/java/com/projects/springbootproject6/StudentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.projects.springbootproject6;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface StudentRepo extends JpaRepository<Student,Long> {
-
-}
