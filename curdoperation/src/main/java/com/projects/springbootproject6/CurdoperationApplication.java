dbb5017a19b478b69be22faf962db20007e1ac74
surendrareddy235 <157972541+surendrareddy235@users.noreply.github.com>
2025-01-30 21:58:24 +0530

    Add files via upload

---
+++ curdoperation/src/main/java/com/projects/springbootproject6/CurdoperationApplication.java
@@ -0,0 +1,13 @@
+package com.projects.springbootproject6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdoperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdoperationApplication.class, args);
+	}
+
+}
